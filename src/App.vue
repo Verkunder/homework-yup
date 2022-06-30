c98de4f1612bb97ef0dea2c8f80420eb2c4e7631
VerkunderWP <s.kapustin@webpractik.ru>
2022-06-30 16:59:10 +0300

    feat(yup): закончил

--- src/App.vue
+++ src/App.vue
@@ -8,33 +8,37 @@
       </div>
       <div class="mb-3">
         <label for="new-pass" class="form-label">Новый пароль</label>
-        <input type="text" class="form-control" id="new-pass">
+        <input type="password" class="form-control" id="new-pass" v-model="newPassword" @blur="nBlur">
+        <small v-if="nError">{{nError}}</small>
       </div>
       <div class="mb-3">
         <label for="re-pass" class="form-label">Повторите пароль</label>
-        <input type="text" class="form-control" id="re-pass">
+        <input type="password" class="form-control" id="re-pass" v-model="confirmPassword" @blur="confirmBlur">
+        <small v-if="confirmError">{{confirmError}}</small>
       </div>
       <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Согласен на все подписки*</label>
+        <input type="checkbox" class="form-check-input" id="checked" v-model="checked" @blur="cBlur">
+        <label class="form-check-label" for="checked">Согласен на все подписки*</label>
+
+        <div v-if="cError">* {{cError}}</div>
       </div>
       <div class="mb-3">
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Роль на платформе</option>
+      <select class="form-select" aria-label="Default select example" v-model="role" @blur="roleBlur">
         <option value="participant">Участник</option>
         <option value="speaker">Выступающий</option>
         <option value="media">СМИ</option>
       </select>
+        <div v-if="roleError">* {{roleError}}</div>
       </div>
       <div class="mb-3">
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Профессия</option>
+        <select class="form-select" aria-label="Default select example" @blur="pBlur" v-model="prof">
           <option value="designer">Дизайнер</option>
           <option value="accountant">Счетовод</option>
           <option value="manager">Менеджер</option>
         </select>
+        <div v-if="profError">* {{profError}}</div>
       </div>
-      <button type="submit" class="btn btn-primary"> Отправить {{isSubmitting}} </button>
+      <button type="submit" class="btn btn-primary"> Отправить </button>
     </form>
   </div>
 </template>
@@ -42,7 +46,28 @@
 <script setup>
 import {useRequestForm} from "../src/use/request-form";
 
-const {password, pError, pBlur, onSybmit, isSubmitting} = useRequestForm()
+const {
+  password,
+  pError,
+  pBlur,
+  onSybmit,
+  isSubmitting,
+  checked,
+  cBlur,
+  cError,
+  nBlur,
+  nError,
+  newPassword,
+  confirmError,
+  confirmBlur,
+  confirmPassword,
+  role,
+  roleBlur,
+  roleError,
+  profError,
+  prof,
+  profBlur
+} = useRequestForm()
 
 </script>
 
--- src/use/request-form.js
+++ src/use/request-form.js
@@ -8,9 +8,62 @@ export function useRequestForm(fn) {
         yup
             .string()
             .trim()
-            .required('Это поле обязательное')
-            .min(6,'* 6 Минимальное количество символов')
+            .when('password', () => {
+                if (newPassword.value || password.value || confirmPassword.value) return yup.string()
+                    .required('Это поле обязательно')
+                    .min(8, 'Минимальное количество символов 8')
+                    .matches(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 'Password can only contain Latin letters.')
+                    .test('confirmPassword', 'Пароли не совпадают', confirmP => newPassword.value === password.value && password.value === confirmPassword.value)
+            })
     )
+    const {value: newPassword, errorMessage: nError, handleBlur: nBlur} = useField(
+        'newPassword',
+        yup
+            .string()
+            .trim()
+            .when('password', () => {
+                if (newPassword.value || password.value || confirmPassword.value) return yup.string()
+                    .required('Это поле обязательно')
+                    .min(8, 'Минимальное количество символов 8')
+                    .matches(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 'Password can only contain Latin letters.')
+                    .test('confirmPassword', 'Пароли не совпадают', confirmP => newPassword.value === password.value && password.value === confirmPassword.value)
+            })
+    )
+
+    const {value: confirmPassword, errorMessage: confirmError, handleBlur: confirmBlur} = useField(
+        'confirmPassword',
+        yup
+            .string()
+            .trim()
+            .when('newPassword', () => {
+                if (newPassword.value || password.value || confirmPassword.value) return yup.string()
+                    .required('Это поле обязательно')
+                    .min(8, 'Минимальное количество символов 8')
+                    .matches(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 'Пароль должен содержать 1 большу 1 маленьку букву и цифры')
+                    .test('confirmPassword1', 'Пароли не совпадают', confirmP => newPassword.value === password.value && password.value === confirmPassword.value)
+            })
+    )
+
+    const {value: checked, errorMessage: cError, handleBlur: cBlur} = useField(
+        'checkbox',
+        yup
+            .boolean()
+            .required('Это обязательно')
+    )
+    const {value: role, errorMessage: roleError, handleBlur: roleBlur} = useField(
+        'role',
+        yup
+            .string()
+            .required('Это обязательно')
+    )
+    const {value: prof, errorMessage: profError, handleBlur: profBlur} = useField(
+        'prof',
+        yup
+            .string()
+            .required('Это обязательно')
+            .test('prof', 'Менеджер не валидное значение!', testManager => role.value === 'media' && prof.value === 'manager' ? false : true)
+    )
+
     const onSybmit = handleSubmit( async values => {
         try {
             console.log(values)
@@ -19,10 +72,25 @@ export function useRequestForm(fn) {
         }
     })
     return {
+        confirmPassword,
+        confirmError,
+        confirmBlur,
         password,
         pError,
         pBlur,
         onSybmit,
-        isSubmitting
+        isSubmitting,
+        checked,
+        cError,
+        cBlur,
+        newPassword,
+        nError,
+        nBlur,
+        role,
+        roleError,
+        roleBlur,
+        prof,
+        profError,
+        profBlur
     }
 }
